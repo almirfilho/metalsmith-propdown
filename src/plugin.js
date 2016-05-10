7eaeca6e9ac76e44e056fb44abfaefc001b582fd
Almir Filho <almirflorenciofilho@gmail.com>
2016-05-10 14:41:29 -0300

    Add property convertion

--- src/plugin.js
+++ src/plugin.js
@@ -1,6 +1,11 @@
+const marked = require('marked');
+
+
 module.exports = options => {
   options = options || {};
+
   if(!options.collection) throw new Error('`collection` option is required.');
+  if(!options.property) throw new Error('`property` option is required.');
 
   return function(files, metalsmith, done){
     var metadata = metalsmith.metadata();
@@ -10,6 +15,13 @@ module.exports = options => {
 
     if(!(options.collection in metadata.collections))
       throw new Error(`the collection '${options.collection}' does not exist.`);
+
+    const collection = options.collection;
+    const property = options.property;
+
+    metadata.collections[collection].forEach(file => {
+      if(property in file) file[property] = marked(file[property]);
+    });
 
     done();
   };
--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect;
 const metalsmith = require('metalsmith');
-const author = require('../src/plugin');
+const collections = require('metalsmith-collections');
+const propdown = require('../src/plugin');
 
 
 describe('Initialization', () => {
@@ -9,32 +10,83 @@ describe('Initialization', () => {
   before(() => metal = metalsmith('test/fixtures'));
 
   it('should be a function', () => {
-    expect(author).to.be.a.function;
+    expect(propdown).to.be.a.function;
   });
 
   it('should return a function', () => {
-    expect(author({ collection: 'something' })).to.be.a.function;
+    expect(propdown({
+      collection: 'collectionname',
+      property: 'propertyname' }
+    )).to.be.a.function;
   });
 
   it('should require collection name', () => {
-    expect(author).to.throw(Error);
+    expect(propdown.bind(null, { property: 'name' })).to.throw(Error);
+  });
+
+  it('should require property name', () => {
+    expect(propdown.bind(null, { collection: 'name' })).to.throw(Error);
   });
 
   it('should require collections to be configured', () => {
     metal._metadata = {};
-    const call = author({ collection: 'name' }).bind(null, [], metal, () => {});
+    const call = propdown({
+      collection: 'collectionname',
+      property: 'propertyname'
+    }).bind(null, [], metal, () => {});
+
     expect(call).to.throw(Error);
   });
 
   it('should throw error if collection doesnt exist', () => {
     metal._metadata.collections = {};
-    const call = author({ collection: 'posts' }).bind(null, [], metal, () => {});
+    const call = propdown({
+      collection: 'posts',
+      property: 'propertyname'
+    }).bind(null, [], metal, () => {});
+
     expect(call).to.throw(Error);
   });
 
   it('should not throw error if collection exist', () => {
     metal._metadata.collections = { articles: [] };
-    const call = author({ collection: 'articles' }).bind(null, [], metal, () => {});
+    const call = propdown({
+      collection: 'articles',
+      property: 'propertyname'
+    }).bind(null, [], metal, () => {});
+
     expect(call).not.to.throw(Error);
+  });
+});
+
+
+describe('Generation', () => {
+  var metalpipeline = propdownoptions => (
+    metalsmith('test')
+      .source('fixtures')
+      .use(collections({ posts: '*.html' }))
+      .use(propdown(propdownoptions))
+  );
+
+  it('should do nothing if property doesnt exist in file', done => {
+    metalpipeline({
+      collection: 'posts',
+      property: 'propertyname'
+    }).build((err, files) => {
+      expect(files['post1.html']).to.have.property('excerpt', 'Some __markdown__ content to _convert_ from post 1.');
+      expect(files['post2.html']).to.have.property('excerpt', 'Some __markdown__ content to _convert_ from post 2.');
+      done();
+    });
+  });
+
+  it('should convert property value from markdown to html', done => {
+    metalpipeline({
+      collection: 'posts',
+      property: 'excerpt'
+    }).build((err, files) => {
+      expect(files['post1.html']).to.have.property('excerpt').to.contain('<em>', '<strong>', 'post 1');
+      expect(files['post2.html']).to.have.property('excerpt').to.contain('<em>', '<strong>', 'post 2');
+      done();
+    });
   });
 });
